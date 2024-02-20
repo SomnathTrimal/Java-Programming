de8e9e8bab599a4549d1921500990df13081dbf4
SomnathTrimal <116556845+SomnathTrimal@users.noreply.github.com>
2024-02-20 23:28:00 +0530

    Add files via upload

---
+++ ConvertCharToLong.java
@@ -0,0 +1,16 @@
+class ConvertCharToLong 
+{  public static void main(String[]args)
+    { 
+        char charValue = 'T';
+
+        long longValue = charValue;
+
+        System.out.println("char value: " + charValue);
+        System.out.println("long value: "+ longValue);
+    }
+
+    
+
+
+
+}
