ad82fa63c43e7accdff9a6f352c19028316ca788
SomnathTrimal <116556845+SomnathTrimal@users.noreply.github.com>
2024-02-21 22:58:21 +0530

    Add files via upload

---
+++ convertLongToFloat.java
@@ -0,0 +1,24 @@
+class convertLongToFloat
+{ 
+    public static void main(String[]args)
+    {
+        Long l1 = 69l;
+        Long l2 = 54l;
+        Long l3  = 12l;
+      
+        float f1 = l1.floatValue();
+        float f2 = l2.floatValue();
+        float f3 = l3.floatValue();
+      System.out.println("the float value for the Long value '"+l1+"' is given as : "+f1);  
+      System.out.println("the float value for the Long value '"+l2+"' is given as : "+f2);  
+      System.out.println("the float value for the Long value '"+l3+"' is given as : "+f3);  
+
+
+
+    }
+
+
+}
+
+
+
